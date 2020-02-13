6dd7bb9a9270bc8436d53505180bf724e115d14e
Emilie Tassé <46505135+emzzzt@users.noreply.github.com>
2020-02-13 13:56:52 -0500

    Update form_2.php

--- form_2.php
+++ form_2.php
@@ -1,44 +1,76 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="utf-8">
-	<title>Form RegEx</title>
-	<!--Faire un formulaire de don qui demande à un usager :
-	php  '%[$"\'\n\d/\\\\]%'  '/regex/'  [1-9] [0-9] preg function
-	whenever you want to do something with a regular expression, you have to pass it as a string
-	to one of the preg functions
-	preg_match() returns 1 if a match can be found and 0 when the regex cannot match the subject at all
-	there is also preg_match_all() it will find all matche in the string iterate over the array with the
-	match result.
-	left to right
-	Limit input to alphanumeric charcaters
-	application requires thats users limit their responses to one or more alphanumeric English 
-	charaters
-	Strip leading zeros you want to match an integer number, and either return the number
-	without any leading zeros or delete the leading zeros
-	$result = preg_replace('/\b0*([1-9][0-9]*|0)\b/', '$1', $subject);    
-	Numbers for a certain range 1-100 
-	
-a) son numéro d'assurance sociale et
+
+
+<?php
+	$nas = "";
+	$amnt = "";
+	$msg_nas = "";
+	$msg_amnt = "";
+	$valid_nas = false;
+	$valid_amnt = false;
+	// formulaire
+	if(isset($_POST["nas"]) && isset($_POST["amnt"]))
+	{ //tout doit etre valide ici avant de rediriger vers answer.php pour echo le message de remerciement
+		//echo "ok!"; //echo pour tester si ma page fonctionnait
+		$nas = $_POST["nas"]; // valider que la bonne chose est appellée
+
+		$reg_ex_nas = "/\d{3} \d{3} \d{3}/"; // regex pour que les 9 chiffres soient séparés en 3 blocks
+
+		$resultat = preg_match($reg_ex_nas, $nas);
+		//preg_match() returns 1 if a match can be found and 0 when the regex cannot match the subject at all
+		if($resultat === 1) 
+		{
+				# code... l'input sera invalide 
+			$msg_nas = "Le numéro d'assurance social devrait être sous format XXX XXX XXX";
+		}
+		else
+		{
+			$valid_nas = true;
+		}
+
+		$amnt = $_POST["amnt"];
+
+		$reg_ex_amnt = "/^[1-9]?\d\.\d\d$/";
+
+		$resultat = preg_match($reg_ex_amnt, $amnt);
+		//preg_match() returns 1 if a match can be found and 0 when the regex cannot match the subject at all
+		if($resultat === 1) 
+		{
+			# code... l'input sera invalide
+			$msg_amnt = "Le montant n'est pas valide";
+		}
+		else
+		{
+			$valid_amnt = true; 
+		}
+	}
+
+	if ($valid_nas && $valid_amnt) 
+	{
+		# code...Rediriger
+		header("Location: answer.html");
+	}
+
+
+?>
+
+<!--	a) son numéro d'assurance sociale et
 
 b) un montant valide entre 0$ et 99.99$. 
 
 Par exemple, 01.20$ n'est pas valide. 10.1$ non plus. 1.20$ et 10.10$ sont valides.
 
 Valider les infos. Si tout est valide, faire une redirection vers une seconde page qui remercie l'usager pour le don. Si tout n'est pas valide, afficher les messages d'erreur appropriés. -->
-</head>
-<?php
-	if (isset($_POST['myForm'])) { //tout doit etre valide ici avant de rediriger vers answer.php pour echo le message de remerciement
-		echo "ok!";
-	}
-?>
-<body>
-		<form action="answer.php" method="post">
+<html>
+	<head>
+		<title>Form RegEx</title>
+	</head>
+	<body>
+		<form method="post">
 			Entrez votre Numéro d'assurance social : 
-			<input type="password" name="nas" ><br><br>
+			<input type="text" name="nas" value="<?= $nas ?>"/><span><?= $msg_nas ?></span><br><br>
 			Entrez un montant : 
-			<input type="text" name="amnt"><br><br>
-			<input type="submit" name="myForm" value="Envoyer">
+			<input type="text" name="amnt" value="<?= $amnt ?>"/><span><?= $msg_amnt ?></span><br><br>
+			<input type="submit" name="myForm" value="Envoyer"/>
 		</form>
-</body>
+	</body>
 </html>
