cf93796a9029dacaa14f86a9ce384ff87d2792ec
Emilie Tassé <46505135+emzzzt@users.noreply.github.com>
2020-02-13 14:58:21 -0500

    Update answer.php

--- answer.php
+++ answer.php
@@ -4,12 +4,8 @@
 	<title>Merci pour votre don!</title>
 </head>
 <body>
-	<?php
-		//if ($variable qui store le regex pour le montant du don) {
-			# code...//vérifier si le regex pour le montant est exact
-		//}else{
-			echo "<h3>Merci pour votre don!</h3>";
-		//}
-	?>
+	
+	<h3>Merci pour votre don!</h3>
+	
 </body>
 </html>
